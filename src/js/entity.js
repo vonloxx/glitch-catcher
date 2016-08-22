ed0f977f30cfe2c0072304aad39c6881ce182e48
Marco Fernandes <marco@front.no>
2016-08-22 16:15:38 +0100

    Build.

--- src/js/entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function entity(options) {
-  var options = {
-
-  }
-}
--- src/js/sprite.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function Sprite(options) {
-  options = {
-    id: 0,
-    test: 3
-  }.merge(options);
-}
